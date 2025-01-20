1f8062946cf6516313f84cadcde4e414cc146068
MarioCadenas <MarioCadenas@users.noreply.github.com>
2025-01-20 14:26:35 +0100

    chore: add pre-commit hooks

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'chore', 'refactor', 'test', 'revert', 'build'],
-    ],
-    'scope-empty': [2, 'never'],
-    'scope-case': [2, 'always', 'pascal-case'],
-    'subject-case': [2, 'always', ['start-case', 'sentence-case']],
-  },
-};
+export default { extends: ['@commitlint/config-conventional'] };
