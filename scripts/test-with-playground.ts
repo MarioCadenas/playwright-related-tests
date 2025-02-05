da74ba2a2d0c98b02ea6ea63c075a1afe56e6b90
gcoronado <gcoronado@newrelic.com>
2025-02-05 17:11:41 +0100

    fix: fix playground

--- scripts/test-with-playground.ts
+++ scripts/test-with-playground.ts
@@ -2,36 +2,35 @@ import { exec as syncExec } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
 import { promisify } from 'node:util';
-// This is a simple script that will run the E2E tests in a playground
-// and help you test new changes made to the library.
+
+const PLAYGROUND_DIR = 'test/playground';
 
 const exec = promisify(syncExec);
+async function executeCommandWithLogs(command: string) {
+  const { stdout, stderr } = await exec(command);
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+  }
+  console.log(`stdout: ${stdout}`);
+}
 
-// Make changes to a component that is used in the E2E tests
-const filePath = 'test/playground/src/components/Home/Home.tsx';
+// Modify a component that is used in the E2E tests
+const filePath = path.join(PLAYGROUND_DIR, 'src/components/Home/Home.tsx');
 const file = fs.readFileSync(filePath, 'utf8').toString();
 const changedFile = file.replace('Home Page', 'Welcome to the Home Page');
 fs.writeFileSync(filePath, changedFile, 'utf8');
 
-// Run the E2E test and check how only the home.spec.js is executed
-const { stdout: e2eResult } = await exec('cd test/playground && npm run e2e');
-console.log(e2eResult);
+// Copy test/playground/fixtures to test/playground/node_modules/.playwright-related-tests
+const affectedFilesFixture = path.join(PLAYGROUND_DIR, 'fixtures');
+const prtConfigFoler = path.join(
+  PLAYGROUND_DIR,
+  'node_modules/.playwright-related-tests',
+);
+await fs.promises.cp(affectedFilesFixture, prtConfigFoler, { recursive: true });
 
-// Restore the modified .affected-files and the component that was modified
-const affectedFilesDir = 'test/playground/.affected-files';
-const paths = [
-  path.join(
-    affectedFilesDir,
-    'about.spec.js - Navigation to About page - should display the page title.json',
-  ),
-  path.join(
-    affectedFilesDir,
-    'home.spec.js - Navigation to Home page - should display the page title.json',
-  ),
-  path.join(
-    affectedFilesDir,
-    'profile.spec.js - Navigation to Profile page - should display the page title.json',
-  ),
-  filePath,
-];
+// Run the E2E test and check how only the home.spec.js is executed
+await executeCommandWithLogs('cd test/playground && npm run e2e');
+
+// Restore the modified component
+const paths = [filePath];
 await exec(`git restore ${paths.map((path) => `"${path}"`).join(' ')}`);
